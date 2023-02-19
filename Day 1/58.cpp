f45c3a8f44e24b0794baf1f9ace40ed41600aa15
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2023-02-19 22:19:31 +0530

    Added Day 1.1

---
+++ Day 1/58.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1; int ct = 0;
+        while(s[i]==' ') i--;
+        for(; i>=0&&s[i]!=' '; i--) ct++;
+        return ct;
+    }
+};
